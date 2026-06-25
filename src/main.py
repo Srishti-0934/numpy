98d6a39d019c1d1676fb263627a99ec511c7a18f
Srishti <srishtiiii.singh3@gmail.com>
2026-06-25 20:06:33 +0530

    initial commit

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+data = np.genfromtxt(
+    "data/students.csv",
+    delimiter=",",
+    skip_header=1
+)
+
+print("\n STUDENT DATA ")
+print(data)
+
+
+student_ids = data[:, 0].astype(int)
+marks = data[:, 1:]
+
+subjects = [
+    "Math",
+    "Physics",
+    "Chemistry"
+]
+
+print("\n===== SUBJECT STATISTICS =====")
+
+for i in range(len(subjects)):
+
+    print(f"\n{subjects[i]}")
+
+    print("-"*25)
+
+    print("Mean :", np.mean(marks[:, i]))
+
+    print("Median :", np.median(marks[:, i]))
+
+    print("Minimum :", np.min(marks[:, i]))
+
+    print("Maximum :", np.max(marks[:, i]))
+
+    print("Variance :", np.var(marks[:, i]))
+
+    print("Standard Deviation :", np.std(marks[:, i]))
